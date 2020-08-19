7412616337cfd0fde14b04be946fbc33ebfd1e16
Ally Brannon <allison.brannon@gmail.com>
2020-08-19 15:54:17 -0400

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const db = require("./db");
 const port = 5544;
-app.use(express.static(__dirname+'/public'));
 
-app.get("/api", (req,res)=>{
-    let page = req.query.page || 1;
-    let limit = req.query.limit || 100;
-    res.send(db.getAll(limit, page))
-})
+const cors = require("cors");
+app.use(cors());
+app.use(express.static(__dirname + "/site"));
 
-app.get("/api/filter/",(req,res)=>{
-    let attrib = req.query.attribute;
-    let value= req.query.value;
-    res.send(db.getByAttribute(attrib, value))
-})
+app.get("/api", (req, res) => {
+  let page = req.query.page || 1;
+  let limit = req.query.limit || 100;
+  res.send(db.getAll(limit, page));
+});
 
-app.get("/api/:id", (req,res)=>{
-    res.send(db.getById(req.params.id))
-})
+app.get("/api/filter/", (req, res) => {
+  let attrib = req.query.attribute;
+  let value = req.query.value;
+  res.send(db.getByAttribute(attrib, value));
+});
 
-app.listen(port, ()=>{
-    console.log(`Listening on http://localhost:${port}`)
-})
+app.get("/api/:id", (req, res) => {
+  res.send(db.getById(req.params.id));
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
